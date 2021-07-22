0305d60bb7d67758e4c8b0c0d3ce969284e9376f
shinaaaa <shin029abc@gmail.com>
2021-07-22 23:04:24 +0900

    operation

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -85,9 +85,16 @@ public class BasicController {
     }
 
     @GetMapping("/literal")
-
     public String literal(Model model) {
         model.addAttribute("data", "Spring!");
         return "basic/literal";
     }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
 }
